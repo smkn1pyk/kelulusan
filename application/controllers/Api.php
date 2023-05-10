7ef2d3a462ea0624057a26f3810fabaa449b989f
yoki <ynrestu@hotmail.com>
2023-05-10 09:36:17 +0700

    1

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -8,25 +8,27 @@ class Api extends RestController {
 		$slice = array_slice($_POST, 1);
 		$decrypt = json_decode(base64_decode($slice['getsekolah']), true);
 		if(is_array($decrypt)){
-			foreach ($decrypt as $key => $value) {
-				$cekData = $this->db->get_where('getsekolah', ['sekolah_id'=>$value['sekolah_id'], 'semester_id'=>$value['semester_id']])->result_array();
-				if($cekData){
-					foreach ($cekData as $key1 => $value1) {
-						$this->db->where($value1);
-						$this->db->update('getsekolah', $value);
-						if($this->db->affected_rows()>>0){
-							$berhasil_tambah[] = 1;
-						}else{
-							$gagal_tambah[] = 0;
-						}
-					}
-				}else{
-					$this->db->insert('getsekolah', $value);
+			// echo "<pre>";
+			// print_r ($decrypt);
+			// echo "</pre>";
+			// exit();
+			$cekData = $this->db->get_where('getsekolah', ['sekolah_id'=>$decrypt['sekolah_id'], 'semester_id'=>$decrypt['semester_id']])->result_array();
+			if($cekData){
+				foreach ($cekData as $key1 => $value1) {
+					$this->db->where($value1);
+					$this->db->update('getsekolah', $decrypt);
 					if($this->db->affected_rows()>>0){
 						$berhasil_tambah[] = 1;
 					}else{
 						$gagal_tambah[] = 0;
 					}
+				}
+			}else{
+				$this->db->insert('getsekolah', $decrypt);
+				if($this->db->affected_rows()>>0){
+					$berhasil_tambah[] = 1;
+				}else{
+					$gagal_tambah[] = 0;
 				}
 			}
 			$response = [
@@ -44,111 +46,141 @@ class Api extends RestController {
 		}
 	}
 
-
-	function pembelajaran_post()
+	function getsekolah_get()
 	{
-		$pembelajaran = json_decode(base64_decode($this->input->post('pembelajaran')),true);
-		if($pembelajaran){
-			foreach ($pembelajaran as $key => $value) {
-				// $this->db->where();
-				$this->db->delete('pembelajaran', ['pembelajaran_id'=>$value['pembelajaran_id']]);
-			}
-			$this->db->insert_batch('pembelajaran', $pembelajaran);
-			if($this->db->affected_rows()>>0){
-				$status = 1;
-			}else{
-				$status = 0;
-			}
-			$this->response(
-				[
-					'status' => true,
-					'message' => "Success",
-					'data' => $status,
-				], 200
-			);
+		if($this->input->get('semester_id')){
+			$cekData = $this->db->get_where('getsekolah', ['semester_id'=>$this->input->get('semester_id')])->result();
+			$response = [
+				'status' => true,
+				'message' => "Success",
+				'data' => $cekData,
+			];
+
+			$this->response($response, 200);
 		}
 	}
 
-	function gtk_post()
+	function getgtk_post()
 	{
-		$getgtk = json_decode(base64_decode($this->input->post('getgtk')),true);
-		if($getgtk){
-
-			foreach ($getgtk as $key => $value) {
-				$where = $value;
-				$this->db->where(['ptk_id'=>$value->ptk_id]);
-				$this->db->delete('getgtk');
-			}
-			$this->db->insert_batch('getgtk', $getgtk);
-			if($this->db->affected_rows()>>0){
-				$status = 1;
+		$slice = array_slice($_POST, 1);
+		$decrypt = json_decode(base64_decode($slice['getgtk']), true);
+		if(is_array($decrypt)){
+			$cekData = $this->db->get_where('getgtk', ['ptk_id'=>$decrypt['ptk_id'], 'tahun_ajaran_id'=>$decrypt['tahun_ajaran_id']])->result_array();
+			if($cekData){
+				foreach ($cekData as $key => $value) {
+					$this->db->where($value);
+					$this->db->update('getgtk', $decrypt);
+					if($this->db->affected_rows()>>0){
+						$berhasil_tambah[] = 1;
+					}else{
+						$gagal_tambah[] = 1;
+					}
+				}
 			}else{
-				$status = 0;
+				$this->db->insert('getgtk', $decrypt);
+				if($this->db->affected_rows()>>0){
+					$berhasil_tambah[] = 1;
+				}else{
+					$gagal_tambah[] = 1;
+				}
 			}
-			$this->response(
-				[
-					'status' => true,
-					'message' => "Success",
-					'data' => $status,
-				], 200
-			);
-		}
-	}
+			$response = [
+				'status' => true,
+				'message' => "Success",
+				'data' => $this->db->last_query(),
+			];
 
-	function rombel_post()
-	{
-		$rombel = json_decode(base64_decode($this->input->post('rombel')),true);
-		if($rombel){
-			foreach ($rombel as $key => $value) {
-				$this->db->delete('getrombonganbelajar', ['rombongan_belajar_id'=>$value['rombongan_belajar_id']]);
-			}
-			$this->db->insert_batch('getrombonganbelajar', $rombel);
-			if($this->db->affected_rows()>>0){
-				$status = 1;
-			}else{
-				$status = 0;
-			}
-			$this->response(
-				[
-					'status' => true,
-					'message' => "Success",
-					'data' => $status,
-				], 200
-			);
-		}
-	}
-
-	function siswa_post()
-	{
-		if($this->input->post('siswa')){
-			$siswa = json_decode(base64_decode($this->input->post('siswa')),true);
-			foreach ($siswa as $key => $value) {
-				$this->db->delete('getpesertadidik',['peserta_didik_id'=>$value['peserta_didik_id']]);
-				$simpanData[] = $value;
-			}
-			$this->db->insert_batch('getpesertadidik', $simpanData);
-			if($this->db->affected_rows()>>0){
-				$status = 1;
-			}else{
-				$status = 0;
-			}
-			$this->response(
-				[
-					'status' => true,
-					'message' => "Success",
-					'data' => $status,
-				], 200
-			);
+			$this->response($response, 200);
 		}else{
-			$this->response(
-				[
-					'status' => false,
-					'message' => "Error",
-					'data' => $_POST,
-				], 200
-			);
+			$response = [
+				'status' => false,
+				'message' => "Bukan Array",
+			];
+
+			$this->response($response, 200);
 		}
-		
+	}
+
+	function rwy_pend_formal_post()
+	{
+		$slice = array_slice($_POST, 1);
+		$decrypt = json_decode(base64_decode($slice['rwy_pend_formal']), true);
+		if(is_array($decrypt)){
+			$cekData = $this->db->get_where('rwy_pend_formal', ['riwayat_pendidikan_formal_id'=>$decrypt['riwayat_pendidikan_formal_id'], 'semester_id'=>$decrypt['semester_id']])->result_array();
+			if($cekData){
+				foreach ($cekData as $key => $value) {
+					$this->db->where($value);
+					$this->db->update('rwy_pend_formal', $decrypt);
+					if($this->db->affected_rows()>>0){
+						$berhasil_tambah[] = 1;
+					}else{
+						$gagal_tambah[] = 1;
+					}
+				}
+			}else{
+				$this->db->insert('rwy_pend_formal', $decrypt);
+				if($this->db->affected_rows()>>0){
+					$berhasil_tambah[] = 1;
+				}else{
+					$gagal_tambah[] = 1;
+				}
+			}
+			$response = [
+				'status' => true,
+				'message' => "Success",
+				'data' => $this->db->last_query(),
+			];
+
+			$this->response($response, 200);
+		}else{
+			$response = [
+				'status' => false,
+				'message' => "Bukan Array",
+			];
+
+			$this->response($response, 200);
+		}
+	}
+
+	function rwy_kepangkatan_post()
+	{
+		$slice = array_slice($_POST, 1);
+		$decrypt = json_decode(base64_decode($slice['rwy_kepangkatan']), true);
+		if(is_array($decrypt)){
+			$cekData = $this->db->get_where('rwy_kepangkatan', ['riwayat_kepangkatan_id'=>$decrypt['riwayat_kepangkatan_id'], 'semester_id'=>$decrypt['semester_id']])->result_array();
+			if($cekData){
+				foreach ($cekData as $key => $value) {
+					$this->db->where($value);
+					$this->db->update('rwy_kepangkatan', $decrypt);
+					if($this->db->affected_rows()>>0){
+						$berhasil_tambah[] = 1;
+					}else{
+						$gagal_tambah[] = 1;
+					}
+				}
+			}else{
+				$this->db->insert('rwy_kepangkatan', $decrypt);
+				if($this->db->affected_rows()>>0){
+					$berhasil_tambah[] = 1;
+				}else{
+					$gagal_tambah[] = 1;
+				}
+			}
+			$response = [
+				'status' => true,
+				'message' => "Success",
+				'data' => $this->db->last_query(),
+			];
+
+			$this->response($response, 200);
+		}else{
+			$response = [
+				'status' => false,
+				'message' => "Bukan Array",
+			];
+
+			$this->response($response, 200);
+		}
 	}
 
 	function getpesertadidik_post()
@@ -156,28 +188,163 @@ class Api extends RestController {
 		$slice = array_slice($_POST, 1);
 		$decrypt = json_decode(base64_decode($slice['getpesertadidik']), true);
 		if(is_array($decrypt)){
-			foreach ($decrypt as $key => $value) {
-				foreach ($value as $key1 => $value1) {
-					foreach ($value1 as $key2 => $value2) {
-						// foreach ($value2 as $key3 => $value3) {
-						$trimmed_array = array_map('trim', $value2);
-						$insert = [$key2=>$trimmed_array];
-						// }
-						$this->db->insert_batch('getpesertadidik', $insert);
+			$cekData = $this->db->get_where('getpesertadidik', ['peserta_didik_id'=>$decrypt['peserta_didik_id'], 'semester_id'=>$decrypt['semester_id']])->result_array();
+			if($cekData){
+				foreach ($cekData as $key => $value) {
+					$this->db->where($value);
+					$this->db->update('getpesertadidik', $decrypt);
+					if($this->db->affected_rows()>>0){
+						$berhasil_tambah[] = 1;
+					}else{
+						$gagal_tambah[] = 1;
 					}
 				}
+			}else{
+				$this->db->insert('getpesertadidik', $decrypt);
+				if($this->db->affected_rows()>>0){
+					$berhasil_tambah[] = 1;
+				}else{
+					$gagal_tambah[] = 1;
+				}
 			}
-		}else{
-			echo "Bukan Array";
-		}
-
-		$this->response(
-			[
-				'status' => false,
-				'message' => "Error",
+			$response = [
+				'status' => true,
+				'message' => "Success",
 				'data' => $this->db->last_query(),
-			], 200
-		);
+			];
+
+			$this->response($response, 200);
+		}else{
+			$response = [
+				'status' => false,
+				'message' => "Bukan Array",
+			];
+
+			$this->response($response, 200);
+		}
+	}
+
+	function getrombonganbelajar_post()
+	{
+		$slice = array_slice($_POST, 1);
+		$decrypt = json_decode(base64_decode($slice['getrombonganbelajar']), true);
+		if(is_array($decrypt)){
+			$cekData = $this->db->get_where('getrombonganbelajar', ['rombongan_belajar_id'=>$decrypt['rombongan_belajar_id'], 'semester_id'=>$decrypt['semester_id']])->result_array();
+			if($cekData){
+				foreach ($cekData as $key => $value) {
+					$this->db->where($value);
+					$this->db->update('getrombonganbelajar', $decrypt);
+					if($this->db->affected_rows()>>0){
+						$berhasil_tambah[] = 1;
+					}else{
+						$gagal_tambah[] = 1;
+					}
+				}
+			}else{
+				$this->db->insert('getrombonganbelajar', $decrypt);
+				if($this->db->affected_rows()>>0){
+					$berhasil_tambah[] = 1;
+				}else{
+					$gagal_tambah[] = 1;
+				}
+			}
+			$response = [
+				'status' => true,
+				'message' => "Success",
+				'data' => $this->db->last_query(),
+			];
+
+			$this->response($response, 200);
+		}else{
+			$response = [
+				'status' => false,
+				'message' => "Bukan Array",
+			];
+
+			$this->response($response, 200);
+		}
+	}
+
+	function anggota_rombel_post()
+	{
+		$slice = array_slice($_POST, 1);
+		$decrypt = json_decode(base64_decode($slice['anggota_rombel']), true);
+		if(is_array($decrypt)){
+			$cekData = $this->db->get_where('anggota_rombel', ['anggota_rombel_id'=>$decrypt['anggota_rombel_id'],'peserta_didik_id'=>$decrypt['peserta_didik_id'], 'semester_id'=>$decrypt['semester_id']])->result_array();
+			if($cekData){
+				foreach ($cekData as $key => $value) {
+					$this->db->where($value);
+					$this->db->update('anggota_rombel', $decrypt);
+					if($this->db->affected_rows()>>0){
+						$berhasil_tambah[] = 1;
+					}else{
+						$gagal_tambah[] = 1;
+					}
+				}
+			}else{
+				$this->db->insert('anggota_rombel', $decrypt);
+				if($this->db->affected_rows()>>0){
+					$berhasil_tambah[] = 1;
+				}else{
+					$gagal_tambah[] = 1;
+				}
+			}
+			$response = [
+				'status' => true,
+				'message' => "Success",
+				'data' => $this->db->last_query(),
+			];
+
+			$this->response($response, 200);
+		}else{
+			$response = [
+				'status' => false,
+				'message' => "Bukan Array",
+			];
+
+			$this->response($response, 200);
+		}
+	}
+
+	function pembelajaran_post()
+	{
+		$slice = array_slice($_POST, 1);
+		$decrypt = json_decode(base64_decode($slice['pembelajaran']), true);
+		if(is_array($decrypt)){
+			$cekData = $this->db->get_where('pembelajaran', ['pembelajaran_id'=>$decrypt['pembelajaran_id'],'ptk_terdaftar_id'=>$decrypt['ptk_terdaftar_id'], 'semester_id'=>$decrypt['semester_id']])->result_array();
+			if($cekData){
+				foreach ($cekData as $key => $value) {
+					$this->db->where($value);
+					$this->db->update('pembelajaran', $decrypt);
+					if($this->db->affected_rows()>>0){
+						$berhasil_tambah[] = 1;
+					}else{
+						$gagal_tambah[] = 1;
+					}
+				}
+			}else{
+				$this->db->insert('pembelajaran', $decrypt);
+				if($this->db->affected_rows()>>0){
+					$berhasil_tambah[] = 1;
+				}else{
+					$gagal_tambah[] = 1;
+				}
+			}
+			$response = [
+				'status' => true,
+				'message' => "Success",
+				'data' => $this->db->last_query(),
+			];
+
+			$this->response($response, 200);
+		}else{
+			$response = [
+				'status' => false,
+				'message' => "Bukan Array",
+			];
+
+			$this->response($response, 200);
+		}
 	}
 
 	function getpengguna_post()
@@ -185,101 +352,41 @@ class Api extends RestController {
 		$slice = array_slice($_POST, 1);
 		$decrypt = json_decode(base64_decode($slice['getpengguna']), true);
 		if(is_array($decrypt)){
-			foreach ($decrypt as $key => $value) {
-				foreach ($value as $key1 => $value1) {
-					foreach ($value1 as $key2 => $value2) {
-						$cekData = $this->db->get_where('getpengguna', $value2)->row_array();
-						if($cekData){
-							$this->db->where($cekData);
-							$this->db->update('getpengguna', $value2);
-						}else{
-							$this->db->insert('getpengguna', $value2);
-						}
+			$cekData = $this->db->get_where('getpengguna', ['pengguna_id'=>$decrypt['pengguna_id'],'status'=>$decrypt['status']])->result_array();
+			if($cekData){
+				foreach ($cekData as $key => $value) {
+					$this->db->where($value);
+					$this->db->update('getpengguna', $decrypt);
+					if($this->db->affected_rows()>>0){
+						$berhasil_tambah[] = 1;
+					}else{
+						$gagal_tambah[] = 1;
 					}
 				}
+			}else{
+				$this->db->insert('getpengguna', $decrypt);
+				if($this->db->affected_rows()>>0){
+					$berhasil_tambah[] = 1;
+				}else{
+					$gagal_tambah[] = 1;
+				}
 			}
-		}else{
-			echo "Bukan Array";
-		}
-
-		$this->response(
-			[
-				'status' => false,
-				'message' => "Error",
+			$response = [
+				'status' => true,
+				'message' => "Success",
 				'data' => $this->db->last_query(),
-			], 200
-		);
-	}
+			];
 
-	function verval_alamat1_get()
-	{
-		$this->db->order_by('id', 'asc');
-		$verval_alamat = $this->db->get_where('verval_alamat', ['status'=>0])->result();
-		$this->response(
-			[
-				'status' => true,
-				'message' => "Success",
-				'data' => $verval_alamat,
-			], 200
-		);
-	}
+			$this->response($response, 200);
+		}else{
+			$response = [
+				'status' => false,
+				'message' => "Bukan Array",
+			];
 
-	function verval_alamat_get()
-	{
-		$this->db->order_by('id', 'asc');
-		$verval_alamat = $this->db->get_where('verval_alamat', ['status'=>0])->result();
-		$this->response(
-			[
-				'status' => true,
-				'message' => "Success",
-				'data' => $verval_alamat,
-				'jml' => count($verval_alamat),
-			], 200
-		);
-	}
-
-	function verval_alamat_update_post()
-	{
-		if($this->input->post('peserta_didik_id')){
-			$jml_verval_alamat = $this->db->get_where('verval_alamat', ['status'=>0])->result();
-			$this->db->where('peserta_didik_id', $this->input->post('peserta_didik_id'));
-			$this->db->update('verval_alamat', ['status'=>1]);
-			if($this->db->affected_rows()>>0){
-				$status = 1;
-			}else{
-				$status = 0;
-			}
-			$this->response(
-				[
-					'status' => true,
-					'message' => "Success",
-					'data' => $status,
-					'jml' => count($jml_verval_alamat),
-				], 200
-			);
+			$this->response($response, 200);
 		}
 	}
-
-	function verval_alamat_update_pesan_post()
-	{
-		if($_POST['peserta_didik_id']&&$_POST['pesan']&&$_POST['status']){
-			$this->db->where('peserta_didik_id', $this->input->post('peserta_didik_id'));
-			$this->db->update('verval_alamat', ['status'=>$this->input->post('status'),'pesan'=>$this->input->post('pesan')]);
-			if($this->db->affected_rows()>>0){
-				$status = 1;
-			}else{
-				$status = 0;
-			}
-			$this->response(
-				[
-					'status' => true,
-					'message' => "Success",
-					'data' => $status,
-				], 200
-			);
-		}
-	}
-
 }
 
 /* End of file Api.php */
